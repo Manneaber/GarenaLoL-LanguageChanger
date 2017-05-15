0bdc9bf06be3f7b6762c27fb9d2ed92dd0084e8e
Manneaber <Manneaber@users.noreply.github.com>
2017-05-15 15:27:23 +0700

    V1

---
+++ LoLLCULanguageChanger/LoLLCULanguageChanger/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoLLCULanguageChanger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            args[1] = "--locale=en_US";
+
+            Process p = new Process();
+            p.StartInfo.FileName = "lol.exe";
+            p.StartInfo.Arguments = string.Join(" ", args.Skip(1));
+            p.Start();
+
+            Process.GetCurrentProcess().Kill();
+        }
+    }
+}
