1c9a815de8aebac3c2fa92eba3cf8eec82f8f30b
Manneaber <Manneaber@users.noreply.github.com>
2017-07-09 17:27:09 +0700

    update

--- LoLLCULanguageChanger/LoLLCULanguageChanger/Program.cs
+++ LoLLCULanguageChanger/LoLLCULanguageChanger/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,10 +13,16 @@ namespace LoLLCULanguageChanger
     {
         static void Main(string[] args)
         {
-            args[1] = "--locale=en_US";
+            Console.WriteLine(string.Join(" ", args));
+            Console.Write("Choose your language (blank or 0: TH, 1: EN) : ");
+            string getstr = Console.ReadLine();
+            if (getstr == "1")
+            {
+                args[1] = "--locale=en_US";
+            }
 
             Process p = new Process();
-            p.StartInfo.FileName = "lol.exe";
+            p.StartInfo.FileName = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\LeagueClient\LeagueClient.exe";
             p.StartInfo.Arguments = string.Join(" ", args.Skip(1));
             p.Start();
 
